9ac34ec333487551ea7177c0e29362b1b51e6a7a
Kendall <ksdavis04@hotmail.com>
2021-10-19 18:20:59 -0400

    practice

--- practice.js
+++ practice.js
@@ -1,3 +1,17 @@
-let fullName; 
 
-console.log(fullName); 
+
+var loggedIn = false; 
+
+while (!loggedIn) { 
+    let username = prompt('Username:'); 
+    let password = prompt('Password:');
+
+
+    if (username == 'kendall0404' && password == '040404'){ 
+        alert('Welcome back, ' + username); 
+        loggedIn = true;
+    } else { 
+        alert('Incorrect username of password');
+    }
+    
+}
